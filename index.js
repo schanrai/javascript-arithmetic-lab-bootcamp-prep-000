511966367dd2a61a351f92a43879bd280ca33789
Sushi Chanrai <sushi@pathworks.co>
2018-05-22 03:04:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+var n=557
+function inc(n){
+  return n++;
+}
+inc(n);
+
+    n=7
+        function dec(n){
+         n--
+         return n
+         }
+
+         dec(n);
